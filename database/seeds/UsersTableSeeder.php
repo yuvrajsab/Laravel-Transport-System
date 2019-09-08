9c9eda41f48a0d066f3dde02aba5bc141277de96
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2019-09-08 09:26:54 +0530

    added .env file and created database seeder for default user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+	DB::table('users')->insert([
+		'name' => "Default User",
+		'email' => "root@laravel.com",
+		'password' => bcrypt('password'),
+		'address' => "address",
+		'zip_code' => "111111",
+		'city_id' => 1,
+		'state_id' => 1,	
+	]);
     }
 }
